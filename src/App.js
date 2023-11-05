092bae6af38fc2bfffe0eb617a5c3e46ed4b0e4d
cyrine.eladeb <cyrine.eladeb@MININT-29I9OC0.bank-sud.tn>
2023-11-05 10:26:32 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ArtisanProfile from './Components/ArtisanProfile';
 import ProductListing from './Components/ProductListing';
 import NavigationBar from './Components/NavigationBar';
-import Register from './Components/Register';
+import Home from './Components/Home';
 import Advantages from './Components/Advantages';
 import HIW from './Components/HIW';
 import artisanProfileData from './Data';
 
 function App() {
   return (
-    <BrowserRouter> 
+    <Router>
       <div>
         <NavigationBar />
-        <Switch>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/advantages">
-            <Advantages />
-          </Route>
-          <Route path="/how-it-works">
-            <HIW />
-          </Route>
-          <Route path="/">
-            <ArtisanProfile artisan={artisanProfileData} />
-            <ProductListing products={artisanProfileData.products} />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/advantages" element={<Advantages />} />
+          <Route path="/how-it-works" element={<HIW />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
+  );
+}
+
+function Register() {
+  return (
+    <div>
+      <ArtisanProfile artisan={artisanProfileData} />
+      <ProductListing products={artisanProfileData.products} />
+    </div>
   );
 }
 
 export default App;
+
+
--- src/Components/ProductListing.js
+++ src/Components/ProductListing.js
@@ -3,16 +3,6 @@ import React from 'react';
 function ProductListing({ products }) {
   return (
     <div>
-    <h3>Products:</h3>
-      {products.map((product) => (
-        <div key={product.id}>
-            <h3>{product.name}</h3>
-            <p>{product.description}</p>
-            <p>price:DT {product.price}</p>
-            <img src={product.image} alt={product.name}/>
-        </div>
-        
-      ))}
    
     </div>
   );
